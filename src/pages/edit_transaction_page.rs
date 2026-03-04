0847be4a93aed3e45822a629f82f7c81fb297d36
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-03-03 21:22:06 -0500

    Fixed date picker not showing the last row of days in the DaysInMonth mode if the days in month % days per row == 0.

--- src/pages/edit_transaction_page.rs
+++ src/pages/edit_transaction_page.rs
@@ -169,9 +169,8 @@ pub fn date_picker(
     // days in month information
     let days_in_current_month = current_month.days_in_month(*current_year);
     let days_per_row: u32 = 6;
-    let mut rows: u32 = days_in_current_month / days_per_row;
+    let mut rows: u32 = days_in_current_month / days_per_row + 1;
     let days_in_last_row: u32 = days_in_current_month % days_per_row;
-    if days_in_last_row > 0 { rows += 1; }
 
     //months in year information
 
