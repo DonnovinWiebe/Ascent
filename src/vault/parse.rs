ee139935450c64edabf0f19bf49a3cc8cc05999d
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-05-04 21:45:46 -0400

    Fixed a bug where the ring chart would have little to no spacing between the first and last segment if their ring would be filled perfectly or almost perfectly.

--- src/vault/parse.rs
+++ src/vault/parse.rs
@@ -600,7 +600,7 @@ impl Segment {
     pub fn sum_visual_percent(segments: &[Segment]) -> f32 {
         let mut sum_visual_percentage = segments.iter().map(|s| s.visual_percentage).sum();
         #[allow(clippy::cast_precision_loss)] // the length of segments will always be small
-        let percentage = Segment::SPACING * (segments.len() as f32 - 1.0);
+        let percentage = Segment::SPACING * (segments.len() as f32);
         sum_visual_percentage += percentage;
         sum_visual_percentage
     }
