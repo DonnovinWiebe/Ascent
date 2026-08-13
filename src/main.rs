b4694d1adc0fbc43aceaf7f31f9737658a0a1aaa
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-08-12 23:08:06 -0400

    And another one...

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() -> iced::Result {
     unsafe { std::env::set_var("WGPU_BACKEND", "gl"); }
 
     iced::application(App::new, App::update, App::view)
-        .theme(App::theme)
+        .theme(App::iced_theme)
         .subscription(App::subscription)
         .run()
 }
